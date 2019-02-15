e1ddfaebd5a221af68b0c0bc61a8a57eefe5139d
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 18:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function CreatePost() {
+function createPost() {
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
   let commentsTemplate = _.template(document.getElementById("commentsTemplate").innerHTML);
