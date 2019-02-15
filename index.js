f3ecc2aca72ba2313806fe03b40d9a11a1918229
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-15 17:46:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function CreatePost() {
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  let commentsTemplate = _.template(document.getElementById("commentsTemplate").innerHTML);
+
+  let postTitle = document.getElementById("postTitle").value;
+  let postAuthor = document.getElementById("postAuthor").value;
+  let post = document.getElementById("postBody").value;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+}
